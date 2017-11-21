a9b4dc6ef7d7850d63b1c1122bb3ed9f17481981
master <670752970@qq.com>
2017-11-22 06:23:35 +0800

    modfiy'

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const config = require('./src/config');
 new Vue({
 	el: '#app',
 	data: {
+		flexItems: [],
 		ui: config.ui,
 		output: '',
 		isFullScreen: true
@@ -32,30 +33,24 @@ new Vue({
 			win.close();
 		},
 		chosefile: function(e){
-			
-			// flexItems.splice(0, this.flexItems.length);
-
-			// let files = e.target.files,
-			// 	len,
-			// 	file;
-			// if(files && (len = files.length)){
-			// 	for(let i=0; i<len; i++){
-			// 		file = files[i];
-			// 		flexItems.push(`
-			// 			<div class="flex-close" data-itemid="`+flexItems.length+`">&times;</div>
-			// 			<div class="flex-item-preview">
-			// 				<canvas></canvas>
-			// 			</div>
-			// 			<div class="flex-item-fileinfo">名称：`+file.name+`<br>大小：`+file.size+`<br>尺寸：1280*720</div>
-			// 			<div class="flex-item-progress hide">
-			// 				<progress value="0" max="100"></progress>
-			// 			</div>
-			// 		`);
-			// 	}
-			// }
+			let files = e.target.files,
+				len,
+				file;
+			if(files && (len = files.length)){
+				for(let i=0; i<len; i++){
+					file = files[i];
+					this.flexItems.push({
+						progress: 0,
+						fileinfo: `名称：`+file.name+`<br>大小：`+file.size+`<br>尺寸：1280*720`
+					});
+				}
+			}
 		},
 		chosedir: function(e){
             this.output = config.ui.saveas + (e.target.files[0].path || '');
+        },
+        flexItemFn: function(e){
+        	this.flexItems.splice(e,1);
         },
         startConvert: function(){
         	console.log('start');
