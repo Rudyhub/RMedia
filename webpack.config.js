ed42b91e9231fbb54086bbf890be80684224ec72
Rudy <670752970@qq.com>
2018-03-09 19:04:35 +0800

    n

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         filename: "index.js"
     },
     target: 'node-webkit',
-    plugins: [
-        new BabiliPlugin()
-    ]
+    // plugins: [
+    //     new BabiliPlugin()
+    // ]
 };
